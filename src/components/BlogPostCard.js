b82d20b92b44114d61b4348153fd8f7a50648590
Leare Song <leare@nationbetter.uk>
2020-05-07 16:56:29 +0100

    refactoring BlogPostPage to include relevant articles only

--- src/components/BlogPostCard.js
+++ src/components/BlogPostCard.js
@@ -9,20 +9,23 @@ import Hero from '../components/Hero';
 import ProjectImg from '../assets/images/macbook-setup.jpeg';
 import JavaBlogPostImg from '../assets/images/java-oracle-icon.png'
 
-function BlogPostCard(state) {
+function BlogPostCard(props) {
 
     return(
-        <a href="/blog/lets-talk-about-java">
+        <a href={props.link}>
                 <Card lg={4} md={4} sm={12} className="shadow-sm bg-white g-blog-cardx" >
                     <Card.Img className="g-blog-image" variant="top" src={JavaBlogPostImg} alt="Blog post image" />
                     <Card.Body className="d-flex flex-column">
                         <div className="d-flex mb-2 justify-content-between">
-                        <Card.Title className="mb-0 font-weight-bold">Let's talk about Java</Card.Title>
-                        <Badge pill variant="light">Technology</Badge>
+                        <Card.Title className="mb-0 font-weight-bold">{props.title}</Card.Title>
+                        <Badge pill variant="light">{props.category}</Badge>
                         </div>
                         <Card.Text className="text-secondary mb-0">
-                            Codebar talk on java
-                        </Card.Text>                        
+                            {props.subTitle}
+                        </Card.Text> 
+                        {/* <Card.Text className="h-3">
+                            {props.content}
+                        </Card.Text>                        */}
                     </Card.Body>
                     <Card.Footer>
                         <small className="text-muted"> Posted date: 20 April 2020</small>
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,34 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
-
+import Axios from 'axios';
 
 import Hero from '../components/Hero';
 import ProjectImg from '../assets/images/macbook-setup.jpeg';
 import JavaBlogPostImg from '../assets/images/java-oracle-icon.png'
-
+import BlogPost from '../components/BlogPost'
 import BlogPostCard from '../components/BlogPostCard';
 
 function BlogPage(props){
+
+    const [blogposts, setBlogPosts] = useState([]);
+
+    useEffect(() => {
+        Axios.get('http://localhost:8080/blogs')
+        .then(res => {
+            setBlogPosts(res.data);
+            console.log(blogposts);
+        })  
+        .catch(err => {
+          console.error(err);
+        })
+    })
+
+    // useEffect(async() => {
+    //     try {
+    //         const res = await Axios.get('http://localhost:8080/blogs');
+    //         setBlogPosts(res.data);
+    //         console.log(blogposts);
+    //     } catch(err) {
+    //         console.error(err);
+    //     }
+    // })
+
     return(
         <div> 
             <Hero title={props.title} subTitle={props.subTitle}/>
             <Container> 
             <CardDeck className="justify-content-around mb-5">
 
-                <BlogPostCard/>
-                <Card className="g-blog-card">
+            
+                 {blogposts.map(function(blogpost){
+                return <div>
+                    <BlogPostCard 
+                        image="JavaBlogPostImg"
+                        title={blogpost.title} 
+                        subTitle={blogpost.subTitle} 
+                        link={blogpost.link}
+                        category={blogpost.category} 
+                        content={blogpost.content}/>
+                    </div>
+                 })
+                 }
+            
+
+                {/* <Card className="g-blog-card">
                     <Card.Img className="g-blog-image" variant="top" src={ProjectImg} alt="Blog post image" />
                 </Card>
                 <Card className="g-blog-card">
                     <Card.Img className="g-blog-image"  variant="top" src={ProjectImg} alt="Blog post image" />
-                </Card>
+                </Card> */}
             </CardDeck>
             </Container>
         </div>
     );
-}
 
+}
 export default BlogPage;
